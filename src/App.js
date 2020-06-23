241f8bd8091e5ebe167035719bac5c5a35a337b5
Jimmy Andersson <jimmyandersson89@gmail.com>
2020-06-24 00:20:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
+
+    function getQuote() {
+        axios
+            .get('https://quotes.rest/qod')
+            .then((res) => {
+                console.log(res);
+                setQuote(res.data.contents.quotes[0].quote);
+                setAuthor(res.data.contents.quotes[0].author);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    return (
+        <div className="App">
+            <h1>Quote of the Day</h1>
+            <p>{quote}</p>
+            <p>- {author}</p>
+            <button onClick={getQuote}>Quote</button>
+        </div>
+    );
 }
 
 export default App;
